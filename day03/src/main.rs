ff1898a337bef1432d143ff4057b59cdcd783c2a
LambTheDev <8612454+LambTheDev@users.noreply.github.com>
2022-12-03 11:26:55 +0200

    add day03 part 2

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -27,19 +27,40 @@ fn part1(input: &str) -> Result<String> {
         let compartment2: HashSet<_> = line.chars().skip(count_half).collect();
         let mut intersection = compartment1.intersection(&compartment2);
         let common_item = intersection.next().unwrap();
-        let ascii = *common_item as u32;
-        let priority = if ascii > 96 {
-            ascii - 96
-        } else {
-            ascii - 64 + 26
-        };
+        let priority = calculate_priority(*common_item);
         priority_sum += priority;
     }
     Ok(priority_sum.to_string())
 }
 
-fn part2(_input: &str) -> Result<String> {
-    Err("not implemented".into())
+fn part2(input: &str) -> Result<String> {
+    let sacks: Vec<_> = input
+        .lines()
+        .map(|s| s.chars().collect::<HashSet<_>>())
+        .collect();
+
+    let mut priority_sum = 0;
+    for group in sacks.chunks(3) {
+        let sack1 = &group[0];
+        let sack2 = &group[1];
+        let sack3 = &group[2];
+
+        let intersection12: HashSet<_> = sack1.intersection(sack2).cloned().collect();
+        let mut intersection123 = intersection12.intersection(sack3);
+        let common_item = intersection123.next().unwrap();
+        let priority = calculate_priority(*common_item);
+        priority_sum += priority;
+    }
+    Ok(priority_sum.to_string())
+}
+
+fn calculate_priority(c: char) -> u32 {
+    let ascii = c as u32;
+    if ascii > 96 {
+        ascii - 96
+    } else {
+        ascii - 64 + 26
+    }
 }
 
 #[cfg(test)]
@@ -58,6 +79,22 @@ mod tests {
         );
         let actual = part1(input)?;
         assert_eq!(actual, "157");
+
+        Ok(())
+    }
+
+    #[test]
+    fn part2_test() -> Result<()> {
+        let input = concat!(
+            "vJrwpWtwJgWrhcsFMMfFFhFp\n",
+            "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\n",
+            "PmmdzqPrVvPwwTWBwg\n",
+            "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\n",
+            "ttgJtRGJQctTZtZT\n",
+            "CrZsJsPPZsGzwwsLwLmpwMDw\n",
+        );
+        let actual = part2(input)?;
+        assert_eq!(actual, "70");
 
         Ok(())
     }
